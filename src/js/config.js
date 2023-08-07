55e04238850ce44f62b39a20ac2bba22964cd5ae
l-esposti <espostileonardo2@gmail.com>
2023-08-07 13:31:49 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '821519f3-47ab-4591-abc6-32365d2d54f3';
+export const MODAL_CLOSE_SEC = 2.5;
